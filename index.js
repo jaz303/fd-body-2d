336ff28c94550b05cc9979be93a2b2beae387afd
Jason Frame <jason@onehackoranother.com>
2014-08-03 23:36:02 +0200

    tabs -> spaces

--- index.js
+++ index.js
@@ -2,75 +2,75 @@ var vec2 = require('fd-vec2');
 
 var types = {};
 
-types.BODY_AABB           	= 1;
-types.BODY_CIRCLE         	= 3;
-types.BODY_POLYGON        	= 5;
-types.BODY_LINE_SEGMENT   	= 7;
-types.BODY_LINE           	= 8;
-types.BODY_H_AXIS         	= 10;
-types.BODY_V_AXIS         	= 12;
+types.BODY_AABB             = 1;
+types.BODY_CIRCLE           = 3;
+types.BODY_POLYGON          = 5;
+types.BODY_LINE_SEGMENT     = 7;
+types.BODY_LINE             = 8;
+types.BODY_H_AXIS           = 10;
+types.BODY_V_AXIS           = 12;
 
-exports.types 				= types;
+exports.types               = types;
 
-exports.AABB 				= AABB;
-exports.Circle 				= Circle;
-exports.Polygon 			= Polygon;
-exports.LineSegment 		= LineSegment;
-exports.Line 				= Line;
-exports.HAxis 				= HAxis;
-exports.VAxis 				= VAxis;
+exports.AABB                = AABB;
+exports.Circle              = Circle;
+exports.Polygon             = Polygon;
+exports.LineSegment         = LineSegment;
+exports.Line                = Line;
+exports.HAxis               = HAxis;
+exports.VAxis               = VAxis;
 
 function AABB(width, height) {
-	this.width = width;
-	this.height = height;
+    this.width = width;
+    this.height = height;
 
-	this.boundOffset = vec2(width / 2, height / 2);
-	this.boundRadiusSq = ((width/2)*(width/2) + (height/2)*(height/2));
-	this.boundRadius = Math.sqrt(this.boundRadiusSq);
+    this.boundOffset = vec2(width / 2, height / 2);
+    this.boundRadiusSq = ((width/2)*(width/2) + (height/2)*(height/2));
+    this.boundRadius = Math.sqrt(this.boundRadiusSq);
 }
 
 AABB.prototype.type = types.BODY_AABB;
 
 function Circle(radius) {
-	this.radius = radius;
+    this.radius = radius;
 
-	this.boundOffset = vec2.zero();
-	this.boundRadius = radius;
-	this.boundRadiusSq = radius * radius;
+    this.boundOffset = vec2.zero();
+    this.boundRadius = radius;
+    this.boundRadiusSq = radius * radius;
 }
 
 Circle.prototype.type = types.BODY_CIRCLE;
 
 function Polygon(points) {
-	this.points = points;
+    this.points = points;
 
-	var maxSq = 0, zero = vec2.zero();
-	for (var i = 0; i < this.points.length; ++i) {
-		var p = this.points[i], d = vec2.distancesq(zero, p);
-		if (d > maxSq) maxSq = d;
-	}
+    var maxSq = 0, zero = vec2.zero();
+    for (var i = 0; i < this.points.length; ++i) {
+        var p = this.points[i], d = vec2.distancesq(zero, p);
+        if (d > maxSq) maxSq = d;
+    }
 
-	this.boundOffset = vec2.zero();
-	this.boundRadiusSq = maxSq;
-	this.boundRadius = Math.sqrt(maxSq);
+    this.boundOffset = vec2.zero();
+    this.boundRadiusSq = maxSq;
+    this.boundRadius = Math.sqrt(maxSq);
 }
 
 Polygon.prototype.type = types.BODY_POLYGON;
 
 function LineSegment(size) {
-	this.size = vec2.clone(size);
+    this.size = vec2.clone(size);
 
-	this.boundOffset = vec2.zero();
-	vec2.midpoint(this.size, this.boundOffset);
+    this.boundOffset = vec2.zero();
+    vec2.midpoint(this.size, this.boundOffset);
 
-	this.boundRadiusSq = this.boundOffset.magnitudesq();
-	this.boundRadius = Math.sqrt(this.boundRadiusSq);
+    this.boundRadiusSq = this.boundOffset.magnitudesq();
+    this.boundRadius = Math.sqrt(this.boundRadiusSq);
 }
 
 LineSegment.prototype.type = types.BODY_LINE_SEGMENT;
 
 function Line(slope) {
-	this.slope = vec2.clone(slope);
+    this.slope = vec2.clone(slope);
 }
 
 Line.prototype.type = types.BODY_LINE;
